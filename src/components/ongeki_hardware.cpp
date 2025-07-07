721695f7feba6410feaf0bedddd3451cacb9fe9e
fuji6000 <fuji3.mark2.no1@gmail.com>
2025-07-07 21:12:03 +0900

    Update ongeki_hardware.cpp

--- src/components/ongeki_hardware.cpp
+++ src/components/ongeki_hardware.cpp
@@ -166,7 +166,7 @@ namespace component {
             rg = false;
 
             for (auto i = 0; i < 10; i++) {
-                auto read = gpio_get(PIN_MAP[i]) ^ 1;
+                auto read = gpio_get(PIN_MAP[i]) ^ PIN_BIT[i];
                 if (read) {
                     data->switches[SWITCH_INDEX[i]] += 1 << SWITCH_OFFSET[i];
                 }
